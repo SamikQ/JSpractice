ef5c0d3ff7429a4c4bd81c2d7d09cd44763d0782
SamikQ <samokhvalov.dmitriy@gmail.com>
2024-03-11 15:20:20 +0200

    updated

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-"use strict";
-
-const numberOfFilms = +prompt("How many movies did you watch?", "");
-
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-
-for (let i = 0; i <= 1; i++) {
-  const a = prompt("last watched movie?", ""),
-    b = prompt("rate it", "");
-  if (a.length === 0 || b.length === 0 || a.length > 50) {
-    i--;
-    alert('type your movies')
-  } else {
-    personalMovieDB.movies[a] = b;
-    alert('thanks');
-  }
-}
-
-function returnNeighboringNumbers(num) {
-  let a = [--num, num, ++num];
-  console.log(a);
-};
-
-returnNeighboringNumbers(5);
-=======
 'use strict';
 
 /* Задание на урок:
@@ -68,4 +36,3 @@ for (let i = 0; i <= 1; i++) {
 }
 
 console.log(personalMovieDB);
->>>>>>> eb5b2b6b91b755521c9a3071f4b434d3ea7122b1
